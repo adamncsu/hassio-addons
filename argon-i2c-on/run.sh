7ceb3b9eca51fa3d4d1dde3fad50c9b85e4a9a43
Adam Johnson <adamcjohnson@gmail.com>
2025-05-30 18:39:29 -0400

    Update run.sh

--- argon-i2c-on/run.sh
+++ argon-i2c-on/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Reading APO register:"
-i2cget -f -y 1 0x1A 0xFE
+echo "Detecting i2c devices:"
+i2cdetect -y 0
+i2cdetect -y 1
+i2cdetect -y 10
+i2cdetect -y 22
 echo "Running i2cset to enable auto power-on"
 i2cset -f -y 1 0x1A 0xFE 0x01
 echo "Reading APO register:"
