24a0054a73d9af864d64cdf0b667309a1edcceda
Adam Johnson <adamcjohnson@gmail.com>
2025-05-30 00:28:30 -0400

    Update run.sh

--- argon-i2c-on/run.sh
+++ argon-i2c-on/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Detecting I2C devices..."
-i2cdetect -y 1 || echo "i2cdetect failed"
 echo "Running i2cset to enable auto power-on..."
-i2cset -y 1 0x01 0xFE 0x01
+i2cset -y 1 0x1A 0xFE 0x01
 echo "Done. You can now remove this add-on."
 sleep 10
