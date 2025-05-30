a33f8de8d78818a787f52400eba4c5e607fe522b
Adam Johnson <adamcjohnson@gmail.com>
2025-05-30 18:35:40 -0400

    Update run.sh

--- argon-i2c-on/run.sh
+++ argon-i2c-on/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Reading APO register:"
-i2cget -y 1 0x1A 0xFE
+i2cget -f -y 1 0x1A 0xFE
 echo "Running i2cset to enable auto power-on"
-i2cset -y 1 0x1A 0xFE 0x01
+i2cset -f -y 1 0x1A 0xFE 0x01
 echo "Reading APO register:"
-i2cget -y 1 0x1A 0xFE
+i2cget -f -y 1 0x1A 0xFE
+echo "Full dump:"
+i2cdump -y 1 0x1a
 echo "Done. You can now remove this add-on."
 sleep 10
